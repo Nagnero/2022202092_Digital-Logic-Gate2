c153a645d50c699b4d75020284d5f237661a8a50
sunwoo <davidyeon123@naver.com>
2023-10-08 15:44:36 +0900

    assignment4: complete

--- Assignment4/alu4/tb_alu4.v
+++ Assignment4/alu4/tb_alu4.v
@@ -1,13 +1,14 @@
 module tb_alu4();
-	reg clk;
-	reg [3:0] tb_a, tb_b;
+	reg clk, reset;
+	reg [3:0] tb_a, tb_b, result_expected;
 	reg [2:0] tb_op;
+	reg c, n, z, v;
 	
 	wire [3:0] tb_result;
 	wire tb_c, tb_n, tb_z, tb_v;
 	
 	reg [31:0] vectornum, errors; // bookkeeping variables
-	reg [3:0] testvectors[10000:0]; // array of testvectors
+	reg [18:0] testvectors[100:0]; // array of testvectors
 	
 	parameter STEP = 10;
 
@@ -16,13 +17,15 @@ module tb_alu4();
 		.c(tb_c), .n(tb_n), .z(tb_z), .v(tb_v));
 
 	// generate clock
-	always #(STEP/2) clk = ~clk;
+	always 
+	begin
+		clk = 1; #(STEP/2); clk = 0; #(STEP/2);
+	end
 
-	// at start of test, load vectors
-	// and pulse reset
+	// at start of test, load vectors and pulse reset
 	initial
 	begin
-		$readmemb("example.txt", testvectors);
+		$readmemb("example.tv", testvectors);
 		vectornum = 0; errors = 0;
 		reset = 1; #27; reset = 0;
 	end
@@ -30,16 +33,27 @@ module tb_alu4();
 	// apply test vectors on rising edge of clk
 	always @(posedge clk)
 	begin
-		#1; {a, b, c, yexpected} =	testvectors[vectornum];
+		#1; {tb_a, tb_b, tb_op, result_expected, c, n, z, v} = testvectors[vectornum];
 	end
-	
-	// apply test vectors on rising edge of clk
-	always @(posedge clk) #1; {a, b, c, yexpected} =	testvectors[vectornum];
  
-	// increment array index and read next testvector
-	vectornum = vectornum + 1;
-	if (testvectors[vectornum] === 4'bx) begin
-		$display("%d tests completed with %d errors", vectornum, errors);
-		$finish;
-	end
+	// check results on falling edge of clk
+	always @(negedge clk)
+		if (~reset) // skip during reset
+		begin 
+			if (tb_result !== result_expected) 
+			begin
+				$display("Error: inputs = %b_%b", tb_a, tb_b);
+				$display("	outputs = %b (%b expected)", tb_result, result_expected);
+				errors = errors + 1;
+			end
+ 
+			// increment array index and read next testvector
+			vectornum = vectornum + 1;
+			// end program when unexpected input
+			if (testvectors[vectornum] === 19'bx) 
+			begin
+				$display("%d tests completed with %d errors", vectornum, errors);
+				$finish;
+			end
+		end
 endmodule
