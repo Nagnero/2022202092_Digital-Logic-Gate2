a78be86cc271014a47c6f8d54a03484d249c5b6f
sunwoo <davidyeon123@naver.com>
2023-11-01 20:27:29 +0900

    assignment8: fifo_cal_addr and it's testbench complete

--- Assignment8/fifo/fifo.v
+++ Assignment8/fifo/fifo.v
@@ -12,14 +12,15 @@ module fifo(clk, reset_n, rd_en, wr_en, d_in, d_out, full, empty, wr_ack, wr_err
 	wire [2:0] tail;
 	wire [2:0] next_tail;
 	wire [3:0] next_data_count;
+	wire we, re;
 	
 	fifo_ns U0_fifo_ns(.wr_en(wr_en), .rd_en(rd_en), .state(state), .data_count(data_count), .next_state(next_state));
 	
 	fifo_cal_addr U1_fifo_cal_addr(.state(state), .head(head), .tail(tail), .data_count(data_count),
-		.we(wr_en), .re(rd_en), .next_head(next_head), .next_tail(next_tail), .next_data_count(next_data_count));
+		.we(we), .re(re), .next_head(next_head), .next_tail(next_tail), .next_data_count(next_data_count));
 		
 	fifo_out U2_fifo_out(.state(state), .data_count(data_count), 
 		.full(full), .empty(empty), .wr_ack(wrack), .wr_err(wr_err), .rd_ack(rd_ack), .rd_err(rd_err));
 		
-	Register_file U3_Register_file(.clk(clk), .reset_n(reset_n), .wAddr(tail), .wData(d_in), .we(wr_en), .rAddr(head), .rData(d_dout));
+	Register_file U3_Register_file(.clk(clk), .reset_n(reset_n), .wAddr(tail), .wData(d_in), .we(we), .rAddr(head), .rData(d_dout));
 endmodule
---
+++ Assignment8/fifo/fifo_cal_addr.v
@@ -0,0 +1,86 @@
+module fifo_cal_addr(state, head, tail, data_count, we, re, next_head, next_tail, next_data_count);
+	input [2:0] state;
+	input [2:0] head;
+	input [2:0] tail;
+	input [3:0] data_count;
+	output reg we, re;
+	output reg [2:0] next_head;
+	output reg [2:0] next_tail;
+	output reg [3:0] next_data_count;
+	
+	parameter INIT = 3'b000;
+	parameter WRITE = 3'b001;
+	parameter READ = 3'b010;
+	parameter WR_ERROR = 3'b101;
+	parameter RD_ERROR = 3'b110;
+	parameter NO_OP = 3'b111;
+	
+	always@(state or head or tail or data_count)
+	begin
+		case(state)
+			INIT: begin // INIT state
+				we = 1'b0;
+				re = 1'b0;
+				next_head = head; // head, tail, and count is same
+				next_tail = tail;
+				next_data_count = data_count;
+			end
+			WRITE: begin // WRITE state
+				we = 1'b1;
+				re = 1'b0;
+				next_head = head; // head is same
+				case (tail) // increase tail by 1
+					3'b000: next_tail = 3'b001;
+					3'b001: next_tail = 3'b010;
+					3'b010: next_tail = 3'b011;
+					3'b011: next_tail = 3'b100;
+					3'b100: next_tail = 3'b101;
+					3'b101: next_tail = 3'b110;
+					3'b110: next_tail = 3'b111;
+					3'b111: next_tail = 3'b000;
+					default: next_tail = 3'bx;
+				endcase
+				next_data_count = data_count + 4'b0001; // increase count
+			end
+			READ: begin
+				we = 1'b0;
+				re = 1'b1;
+				case (head) // increase head by 1
+					3'b000: next_head = 3'b001;
+					3'b001: next_head = 3'b010;
+					3'b010: next_head = 3'b011;
+					3'b011: next_head = 3'b100;
+					3'b100: next_head = 3'b101;
+					3'b101: next_head = 3'b110;
+					3'b110: next_head = 3'b111;
+					3'b111: next_head = 3'b000;
+					default: next_head = 3'bx;
+				endcase
+				next_tail = tail; // tail is same
+				next_data_count = data_count - 4'b0001; // decrease count
+			end
+			WR_ERROR: begin
+				we = 1'b0;
+				re = 1'b0;
+				next_head = head; // head, tail, and count is same
+				next_tail = tail;
+				next_data_count = data_count;
+			end
+			RD_ERROR: begin
+				we = 1'b0;
+				re = 1'b0;
+				next_head = head; // head, tail, and count is same
+				next_tail = tail;
+				next_data_count = data_count;
+			end
+			NO_OP: begin
+				we = 1'b0;
+				re = 1'b0;
+				next_head = head; // head, tail, and count is same
+				next_tail = tail;
+				next_data_count = data_count;
+			end
+		endcase
+	end
+endmodule
+	
